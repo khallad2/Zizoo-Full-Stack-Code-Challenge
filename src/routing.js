44335ca32150624ba27e03b3d9631dc465784776
khallad2 <khallad_3@hotmail.com>
2019-09-11 15:26:52 +0200

    enhance naming

--- src/routing.js
+++ src/routing.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Notfound from "./components/not-fount-404";
 import App from "./App";
 
-export const routes = (
+export const routing = (
     <Router>
         <div>
             <Route path="/" exact component={App}/>
